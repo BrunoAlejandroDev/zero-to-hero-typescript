578030ab662056d54f81bf14c81051cd67d1d830
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-06-14 15:24:09 -0300

    aula 10 tipo boolean

---
+++ modulo-02/10-boolean-type/index.ts
@@ -0,0 +1,24 @@
+/**
+ * descrição: arquivo responsável por ensinar conceitos básicos sobre 'Tipo Boolean'
+ * data: 14/06/2024
+ * doc referência: https://www.typescriptlang.org/docs/handbook/basic-types.html#boolean
+ * Transpilação do arquivo: <Windows> CTRL + SHIFT + B -> tsc: build/watch
+ */
+
+// ==> Exemplo 01
+
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+// ==> Exemplo 02
+
+let concluido: boolean = false;
+
+if (!concluido) {
+    console.log('Tarefa foi concluída com sucesso!')
+} else {
+    console.log('Tarefa Pendente!');
+}
